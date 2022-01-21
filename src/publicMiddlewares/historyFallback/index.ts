802df933de5562cb63bb4d10c0ce707fc8106896
RUIXE-LAPTOP\Ruixe <Ruixe1569898391@outlook.com>
2022-01-21 12:40:15 +0800

    更改 connect-history-api-fallback htmlAcceptHeaders 为 HTML 内容请求

--- src/publicMiddlewares/historyFallback/index.ts
+++ src/publicMiddlewares/historyFallback/index.ts
@@ -8,6 +8,9 @@ import history from 'connect-history-api-fallback'
 const app: Express = express()
 
 // Register history router fallback
-app.use(history({ index: '/index.html' }))
+app.use(history({
+  index: '/index.html',
+  htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
+}))
 
 export default app
